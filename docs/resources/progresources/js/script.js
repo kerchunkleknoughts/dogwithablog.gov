b5f4856d96217d58e5e37c6fec465c13ea646aa2
O11WL1D <theunclekerchunkle@gmail.com>
2025-03-23 16:25:09 -0600

    default commit

--- docs/resources/progresources/js/script.js
+++ docs/resources/progresources/js/script.js
@@ -325,38 +325,316 @@ if(rando>15){
 
 
 
-function displayart(){
+function displayart(artindex){
 
-  var correctform=document.getElementById("headerholder");
+
+  var correctform=document.getElementById("currentart");
   //console.log(correctform.value);
 
-  let fucky=document.createElement('div');
 
-  fucky.innerHTML=
 
+
+
+
+
+
+
+
+
+  let fucky3=document.createElement('div');
+
+  fucky3.innerHTML=
 
   `
-<div class="headstyle">
+ 
+  <div id=currentart>
 
-  <header >
-    <div>
-      <a href="about.html">about</a>
-      <a href="index.html">home</a>
-      <a href="contact.html">socials-contact</a>
-      <a href="music.html">music</a>
-      <a href="drawings.html">art</a>
-      <a href="journal.html">journal</a>
-      <a href="dreamlog.html">dream log</a>
-      <a href="https://users3.smartgb.com/g/g.php?a=s&i=g36-38132-85">guestbook</a>
-      <button onclick="simplereplace('bgm')" id="bgmbt">kill music</button>
-    </div>
+    <h1 id="centered">LATEST ART</h1>
 
-  </header>
-</div>`
+
+
+      <div class="container">
+        <div class="item">
+            <img src="./resources/art/drawings/newest1/abstract1.jpg" id="indicator2"> </img>
+            </div>
+
+        <div class="item">
+            <img src="./resources/art/drawings/newest1/another1.jpg" id="flip-horizontalm1"> </img>
+            </div>
+            
+        
+            <div class="item">
+                <img src="./resources/art/drawings/newest1/another2.jpg" id="indicator2"> </img>
+                </div>
+
+                <div class="item">
+                    <img src="./resources/art/drawings/newest1/bee.jpg" id="indicator2"> </img>
+                    </div>
+                
+                    <div class="item">
+                        <img src="./resources/art/drawings/newest1/beetle.jpg" id="indicator2"> </img>
+                        </div>
+                
+                        <div class="item">
+                            <img src="./resources/art/drawings/newest1/boschpractice1.jpg" id="indicator2"> </img>
+                            </div>
+                            <div class="item">
+                                <img src="./resources/art/drawings/newest1/boschpractice2.jpg" id="indicator2"> </img>
+                                </div>
+                
+                              
+                                    
+      
+                                    <div class="item">
+                                        <img src="./resources/art/drawings/newest1/combine.jpg" id="indicator2"> </img>
+                                        </div>
+                                    
+                                        <div class="item">
+                                            <img src="./resources/art/drawings/newest1/pinsneedles.jpg" id="indicator2"> </img>
+                                            </div>
+                                    
+                                            <div class="item">
+                                                <img src="./resources/art/drawings/newest1/randopractice.jpg" id="indicator2"> </img>
+                                                </div>
+                                    
+                                                <div class="item">
+                                                    <img src="./resources/art/drawings/newest1/scm2.jpg" id="indicator2"> </img>
+                                                    </div>
+                                    
+                                                    <div class="item">
+                                                        <img src="./resources/art/drawings/newest1/scm3.jpg" id="indicator2"> </img>
+                                                        </div>
+                                    
+                                                        <div class="item">
+                                                            <img src="./resources/art/drawings/newest1/scm4.jpg" id="indicator2"> </img>
+                                                            </div>
+                                    
+                                                            <div class="item">
+                                                                <img src="./resources/art/drawings/newest1/scm5.jpg" id="indicator2"> </img>
+                                                                </div>
+                                                                
+                                                                <div class="item">
+                                                                    <img src="./resources/art/drawings/newest1/scm6.jpg" id="indicator2"> </img>
+                                                                    </div>
+                                                                    <div class="item">
+                                                                        <img src="./resources/art/drawings/newest1/scm7.jpg" id="indicator2"> </img>
+                                                                        </div>
+                                                                        <div class="item">
+                                                                            <img src="./resources/art/drawings/newest1/scm8.jpg" id="indicator2"> </img>
+                                                                            </div>
+                                                                            <div class="item">
+                                                                                <img src="./resources/art/drawings/newest1/sketchclubmisc.jpg" id="indicator2"> </img>
+                                                                                </div>
+                                                                             
+                                              
+      
+      </div>
+
+      </div>
+
+
+`
 ;
 
+
+
+
+
+
+
+
+  let fucky2=document.createElement('div');
+
+  fucky2.innerHTML=
+
+  `
+
+   <div id=currentart>
+   <h1 id="centered">OLDER ART</h1>
+
+      
+      <div class="container">
+        <div class="item">
+            <img src="./resources/art/drawings/slightly new 2/collagefinal.jpg" id="indicator2"> </img>
+            </div>
+
+            <div class="item">
+                <img src="./resources/art/drawings/slightly new 2/collageoutline.jpg" id="indicator2"> </img>
+                </div>
+                <div class="item">
+                    <img src="./resources/art/drawings/slightly new 2/designp.jpg" id="indicator2"> </img>
+                    </div>
+                    <div class="item">
+                        <img src="./resources/art/drawings/slightly new 2/dp.jpg" id="indicator2"> </img>
+                        </div>
+                        <div class="item">
+                            <img src="./resources/art/drawings/slightly new 2/gridfinal.jpg" id="indicator2"> </img>
+                            </div>
+                            <div class="item">
+                                <img src="./resources/art/drawings/slightly new 2/gridmp.jpg" id="indicator2"> </img>
+                                </div>
+                                <div class="item">
+                                    <img src="./resources/art/drawings/slightly new 2/randoo.jpg" id="indicator2"> </img>
+                                    </div>
+                                    <div class="item">
+                                        <img src="./resources/art/drawings/slightly new 2/rano1.jpg" id="indicator2"> </img>
+                                        </div>
+                                        <div class="item">
+                                            <img src="./resources/art/drawings/slightly new 2/stilllife.jpg" id="indicator2"> </img>
+                                            </div>
+                                            <div class="item">
+                                                <img src="./resources/art/drawings/slightly new 2/stilllife2.jpg" id="indicator2"> </img>
+                                                </div>
+                                               
+                                              
+      
+      </div>
+      </div>
+
+
+`
+;
+
+
+let fucky1=document.createElement('div');
+fucky1.innerHTML=
+
+`
+
+   <div id=currentart>
+   <h1 id="centered">EVEN OLDER ART</h1>
+
+
+      <div class="container">
+        <div class="item">
+            <img src="./resources/art/drawings/older 3/faces.jpg" id="flip-horizontalm1"> </img>
+            </div>
+            
+
+          
+            <div class="item">
+                <img src="./resources/art/drawings/older 3/faces2.jpg" id="flip-horizontalm1"> </img>
+                </div>
+
+                <div class="item">
+                    <img src="./resources/art/drawings/older 3/fuckedthing.jpg" id="flip-horizontalm1"> </img>
+                    </div>
+
+                    <div class="item">
+                        <img src="./resources/art/drawings/older 3/ghast2hplove.jpg" id="flip-horizontal"> </img>
+                        </div>
+
+                        
+                        <div class="item">
+                            <img src="./resources/art/drawings/older 3/ghasthplove.jpg" id="flip-horizontal"> </img>
+                            </div>
+    
+                    
+
+                            <div class="item">
+                                <img src="./resources/art/drawings/older 3/hl2sketch.jpg" id="flip-horizontalm1"> </img>
+                                </div>
+
+                                <div class="item">
+                                    <img src="./resources/art/drawings/older 3/idk.jpg" id="flip-horizontalm1"> </img>
+                                    </div>
+
+                                    <div class="item">
+                                        <img src="./resources/art/drawings/older 3/keytre.jpg" id="flip-horizontal"> </img>
+                                        </div>
+
+                                        <div class="item">
+                                            <img src="./resources/art/drawings/older 3/lovecraftbeast.jpg" id="flip-horizontal"> </img>
+                                            </div>
+                                
+
+                                            <div class="item">
+                                                <img src="./resources/art/drawings/older 3/me2beersin.jpg" id="flip-horizontalm1"> </img>
+                                                </div>
+
+                                                <div class="item">
+                                                    <img src="./resources/art/drawings/older 3/shandakbirdunkownkadath.jpg" id="flip-horizontalm1"> </img>
+                                                    </div>
+
+                                                    <div class="item">
+                                                        <img src="./resources/art/drawings/older 3/spearhighlandsbs.jpg" id="flip-horizontal"> </img>
+                                                        </div>
+                                            
+                                                        <div class="item">
+                                                            <img src="./resources/art/drawings/older 3/tree.jpg" id="flip-horizontal"> </img>
+                                                            </div>
+
+                                                            <div class="item">
+                                                                <img src="./resources/art/drawings/older 3/villageofthemoonbeastslovecraft.jpg" id="flip-horizontal"> </img>
+                                                                </div>
+
+                                                                <div class="item">
+                                                                    <img src="./resources/art/drawings/older 3/you.jpg" id="flip-horizontal"> </img>
+                                                                    </div>
+
   
-  correctform.replaceWith(fucky);
+                                        
+                                                                             
+                                              
+      
+      </div>
+      </div>
+
+
+`
+;
+
+
+let fucky0=document.createElement('div');
+fucky0.innerHTML=
+
+`
+ 
+ <div id=currentart>
+      <h1 id="centered">ANCIENT ART!</h1>
+
+
+      <div class="container">
+        <div class="item">
+            <img src="./resources/art/drawings/oldest 4/monsterpractice.jpg" id="flip-horizontal"> </img>
+            </div>
+
+            <div class="container">
+                <div class="item">
+                    <img src="./resources/art/drawings/oldest 4/monstpractice2.jpg" id="flip-horizontal"> </img>
+                    </div>
+
+                    <div class="container">
+                        <div class="item">
+                            <img src="./resources/art/drawings/oldest 4/randosketch.jpg" id="indicator2"> </img>
+                            </div>
+
+                            <div class="container">
+                                <div class="item">
+                                    <img src="./resources/art/drawings/oldest 4/soundofpreserverance.jpg" id="flip-horizontal"> </img>
+                                    </div>
+                        
+
+
+    
+                                                                             
+                                              
+      
+      </div>
+      </div>
+
+
+`
+;
+
+
+
+
+let artarray=[fucky0,fucky1,fucky2,fucky3]
+
+
+  
+  correctform.replaceWith(artarray[artindex]);
 
 
   
