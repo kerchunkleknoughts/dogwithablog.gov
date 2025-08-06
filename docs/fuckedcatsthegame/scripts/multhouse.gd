575aa5e3664ab95900ffdcb901c51ed878abba73
kerchunkle <theunclekerchunkle@gmail.com>
2025-08-05 18:36:32 -0600

    default commit

--- docs/fuckedcatsthegame/scripts/multhouse.gd
+++ docs/fuckedcatsthegame/scripts/multhouse.gd
@@ -50,5 +50,5 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 	print("COL!")
 	#howner.state=howner.states.RECHARGE
 	#if(howner.position.x  self.position.x ):
-		howner.state=howner.states.RECHARGE
+	howner.state=howner.states.RECHARGE
 	pass # Replace with function body.
